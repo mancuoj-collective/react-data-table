eb690bd0c1fbb4a2e43149ad5c95a02bcc244116
mancuoj <mancuoj@gmail.com>
2024-05-17 01:46:30 +0800

    chore: update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tasks Table
+
+- https://ui.shadcn.com/examples/tasks
+- https://github.com/shadcn-ui/ui/tree/main/apps/www/app/(app)/examples/tasks
+- https://fakerjs.dev/api/
--- src/utils/tasks.ts
+++ src/utils/tasks.ts
@@ -1,10 +1,10 @@
-import { promises as fs } from 'node:fs'
+import fsp from 'node:fs/promises'
 import path from 'node:path'
 import { z } from 'zod'
 import { taskSchema } from './schema'
 
 export async function getTasks() {
-  const data = await fs.readFile(path.join(__dirname, '../tasks.json'))
+  const data = await fsp.readFile(path.join(__dirname, '../tasks.json'))
   const tasks = JSON.parse(data.toString())
   return z.array(taskSchema).parse(tasks)
 }
