0187c4674ef0d40b2793556a62b46cc054a51c66
mancuoj <mancuoj@gmail.com>
2024-05-27 01:07:03 +0800

    chore: update deps

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,7 +1,6 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons'
 import type * as React from 'react'
 import { DayPicker } from 'react-day-picker'
-
 import { buttonVariants } from '~/components/ui/button'
 import { cn } from '~/utils/cn'
 
--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -3,9 +3,7 @@ import * as React from 'react'
 import { cn } from '~/utils/cn'
 
 const Popover = PopoverPrimitive.Root
-
 const PopoverTrigger = PopoverPrimitive.Trigger
-
 const PopoverAnchor = PopoverPrimitive.Anchor
 
 const PopoverContent = React.forwardRef<
