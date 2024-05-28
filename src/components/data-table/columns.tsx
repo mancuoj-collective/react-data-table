007dc7edf304702ee2e6df9b47facc3aa44927fa
mancuoj <mancuoj@gmail.com>
2024-05-28 23:13:20 +0800

    feat: selector

--- src/components/data-table/columns.tsx
+++ src/components/data-table/columns.tsx
@@ -1,7 +1,27 @@
 import type { ColumnDef } from '@tanstack/react-table'
 import type { Task } from '~/utils/schema'
+import { Checkbox } from '../ui/checkbox'
 
 export const columns: ColumnDef<Task>[] = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        aria-label="Select all"
+        className="translate-y-[2px]"
+        checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && 'indeterminate')}
+        onCheckedChange={(value) => table.toggleAllRowsSelected(!!value)}
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        aria-label="Select row"
+        className="translate-y-[2px]"
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+      />
+    ),
+  },
   {
     accessorKey: 'id',
     header: 'ID',
